bd2b5145d307eed6783fa64fbc24a606df6a9e7f
BouKiCHi <alf_bincyou@hotmail.com>
2026-09-06 11:25:48 +0900

    依存関係の脆弱性対応と0.5.10への更新

--- eslint.config.js
+++ eslint.config.js
@@ -10,7 +10,7 @@ module.exports = [
     languageOptions: {
       parser: tsParser,
       parserOptions: {
-        ecmaVersion: 6,
+        ecmaVersion: 2022,
         sourceType: "module",
       },
     },
